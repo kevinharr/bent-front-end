fc3a899666c35cd8ceeaf9d025c7187119fdd704
kevinharr <kevinharr@verizon.net>
2023-02-27 23:22:02 -0500

    fix BentCard

--- src/components/BentCard/BentCard.tsx
+++ src/components/BentCard/BentCard.tsx
@@ -1,13 +1,17 @@
 import styles from './BentCard.module.css'
 import { Link } from "react-router-dom"
 import { Bent } from '../../types/models'
+import { useParams } from 'react-router-dom'
+
 
 interface BentCardProps {
   bent: Bent;
+  handleDeleteBent: (id: number) => void;
 }
 
 const BentCard = (props: BentCardProps): JSX.Element => {
   const { bent } = props
+  const {id } = useParams()
 
   return (
     <>
@@ -19,16 +23,17 @@ const BentCard = (props: BentCardProps): JSX.Element => {
             <h1>{bent.favoriteMusic}</h1>
           </span>
         </header>
-      </article>
-    <span>
-    
         <>
-          <Link to={`/bents/${id}/edit`} state={bent}>Edit</Link>
-          <button onClick={() => props.handleDeleteBlog(id)}>Delete</button>
+          <Link to={`/bents/${id}/edit`} state={bent}>
+            <button className={styles.editButton}>Edit</button>
+          </Link>
+          <button
+            className={styles.deleteButton}
+            onClick={() => props.handleDeleteBent(bent.id)}
+            >Delete
+          </button>
         </>
-
-     </span>
-    
+      </article>
     </>
    
   )

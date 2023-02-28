30b7c4fc62e9152402513511a18af9a4b92455b1
kevinharr <kevinharr@verizon.net>
2023-02-27 21:43:30 -0500

    fixed BentList

--- src/pages/BentList/BentList.tsx
+++ src/pages/BentList/BentList.tsx
@@ -1,14 +1,29 @@
+//styles
 import styles from './BentList.module.css'
-import BentCard from '../../components/BentCard/BentCard'
+
+//types
 import { Bent } from '../../types/models'
 
-const BentList = (props) => {
+//components
+import BentCard from '../../components/BentCard/BentCard'
+
+interface BentListProps {
+  bents: Bent[];
+}
+
+const BentList = (props: BentListProps): JSX.Element => {
   return (
-    <main className={styles.container}>
-      {props.bents.map((bent: Bent) => (
-        <BentCard key={bent.id} bent={bent} />
-      ))}
-      Bent List
+    <main className={styles.main}>
+      <h1>Here is a list of bents.</h1>
+      {props.bents.length ?
+        <div className={styles.container}>
+          {props.bents.map(bent => (
+          <BentCard key={bent.id} bent={bent} />
+          ))}
+      </div>
+    :
+      <p>No bents added</p>
+  }
     </main>
   )
 }
